28f162406c1d8f3445629f824d9b4c2c2fe07d97
budisusanto1 <132729789+budisusanto1@users.noreply.github.com>
2025-05-16 13:54:07 +0700

    Add files via upload

---
+++ project/app/Views/toko (2).sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 16, 2025 at 08:53 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `toko`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+-- Error reading structure for table toko.barang: #1932 - Table 'toko.barang' doesn't exist in engine
+-- Error reading data for table toko.barang: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `toko`.`barang`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `karyawan`
+--
+-- Error reading structure for table toko.karyawan: #1932 - Table 'toko.karyawan' doesn't exist in engine
+-- Error reading data for table toko.karyawan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `toko`.`karyawan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nota`
+--
+-- Error reading structure for table toko.nota: #1932 - Table 'toko.nota' doesn't exist in engine
+-- Error reading data for table toko.nota: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `toko`.`nota`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+-- Error reading structure for table toko.order: #1932 - Table 'toko.order' doesn't exist in engine
+-- Error reading data for table toko.order: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `toko`.`order`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesanan`
+--
+-- Error reading structure for table toko.pesanan: #1932 - Table 'toko.pesanan' doesn't exist in engine
+-- Error reading data for table toko.pesanan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `toko`.`pesanan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+-- Error reading structure for table toko.user: #1932 - Table 'toko.user' doesn't exist in engine
+-- Error reading data for table toko.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `toko`.`user`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
